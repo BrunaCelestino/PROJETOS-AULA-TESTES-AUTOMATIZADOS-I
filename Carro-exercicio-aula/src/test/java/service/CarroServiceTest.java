66735902f1f82353076b6c3dc90d94dafe557cfa
BrunaMassuchini <brunaccelestino@gmail.com>
2023-03-13 21:34:33 -0300

    exercicio

--- Carro-exercicio-aula/src/test/java/service/CarroServiceTest.java
+++ Carro-exercicio-aula/src/test/java/service/CarroServiceTest.java
@@ -3,88 +3,87 @@ package service;
 import br.com.ada.bdd.model.Carro;
 import org.junit.Test;
 
-
 import static org.junit.Assert.*;
 
 public class CarroServiceTest {
 
     @Test
-    public void acelerar() {
+    public void acelerarAteVelocidadeMaxima() {
         CarroService service = new CarroService();
         Carro carro = new Carro();
-
-        //Given: Um carro ligado
-        service.ligar(carro);
-
-        //When: Acelerar o carro
-        service.acelerar(carro, 100);
-
-        //Then: velocidade aumenta +10
-        assertEquals(10, carro.getVelocidadeAtual());
-    }
-
-    @Test
-    public void acelerarAteMaxima() {
-        CarroService service = new CarroService();
-        Carro carro = new Carro();
-
-        //Given: Um carro ligado e em velocidade máxima
+        // Given: um carro ligado em velocidade maxima
         service.ligar(carro);
         carro.setVelocidadeAtual(100);
 
-        //When: Acelerar o carro
+        // When: acelerar
         service.acelerar(carro, 100);
 
-        //Then: Mantém a velocidade máxima
+        //Then: a velocidade atual tem que ser igual a maxima
         assertEquals(100, carro.getVelocidadeAtual());
+    }
+
+    @Test
+    public void acelerarOCarroDesligado() {
+        CarroService service = new CarroService();
+
+        // Given: um carro desligado
+        Carro carro = new Carro();
+
+        // When: acelerar
+
+        service.acelerar(carro, 100);
+        //Then: velocidade continua zero
+
+        assertTrue(carro.getVelocidadeAtual() == 0);
+    }
+
+    @Test
+    public void mostrarEstadoAtual() {
+        // Given: um carro
+        CarroService service = new CarroService();
+        Carro carro = new Carro("vermelho", "ford", "sedan", 1995, 100);
+
+
+        // When: mostrar estado
+        String estadoAtual = service.estadoAtual(carro);
+        String carroString = "Carro{cor='vermelho', marca='ford', modelo='sedan', ano='1995', ligado=false, " +
+                "velocidadeAtual=0, velocidadeMaxima=100}";
+
+
+        //Then: imprime o carro
+        assertEquals(carroString, estadoAtual);
 
     }
 
     @Test
-    public void frear() {
+    public void frearCarroParado(){
         CarroService service = new CarroService();
         Carro carro = new Carro();
-
-        //Given: Um carro ligado
+        // Given: um carro ligado e parado
         service.ligar(carro);
 
-        //When: Frear o carro
+        // When: frear
         service.frear(carro);
 
-        //Then: mantém a velocidade 0
-        assertEquals(0, carro.getVelocidadeAtual());
+        // Then: a velocidade não diminui
+        assertFalse(carro.getVelocidadeAtual() < 0);
 
     }
 
-    @Test
-    public void desligar() {
-        CarroService service = new CarroService();
-
-        //Given: Um carro desligado
-        Carro carro = new Carro();
-
-        //When: Desligar o carro
-        service.desligar(carro);
-
-        //Then: Ocorre um erro
-        assertEquals("O carro já está desligado", service.desligar(carro));
-
-    }
 
     @Test
-    public void desligarSemFrear() {
+    public void acelerarCarro(){
         CarroService service = new CarroService();
         Carro carro = new Carro();
-
-        //Given: Um carro ligado
+        // Given: um carro ligado e a 20km por hora
         service.ligar(carro);
+        carro.setVelocidadeAtual(20);
+
+        // When: acelerar
         service.acelerar(carro, 100);
 
-        //When: Em velocidade maior que 0
-
-        //Then: Não pode desligar
-        service.desligar(carro);
-        assertTrue(carro.isLigado());
+        // Then: velocidade igual a 30km/h
+        assertEquals(30, carro.getVelocidadeAtual());
 
     }
 
